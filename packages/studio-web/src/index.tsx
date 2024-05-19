4df5ac34d09b464575b65679fbd0614dc692b974
Paul Draghicescu <pdragy@gmail.com>
2024-05-19 00:34:03 -0500

    remove compatibility banner and stories

--- packages/studio-web/src/index.tsx
+++ packages/studio-web/src/index.tsx
@@ -7,10 +7,6 @@ import ReactDOM from "react-dom";
 
 import Logger from "@foxglove/log";
 import type { IDataSourceFactory } from "@foxglove/studio-base";
-import CssBaseline from "@foxglove/studio-base/components/CssBaseline";
-
-import { CompatibilityBanner } from "./CompatibilityBanner";
-import { canRenderApp } from "./canRenderApp";
 
 const log = Logger.getLogger(__filename);
 
@@ -41,32 +37,6 @@ export async function main(getParams: () => Promise<MainParams> = async () => ({
     throw new Error("missing #root element");
   }
 
-  const chromeMatch = navigator.userAgent.match(/Chrome\/(\d+)\./);
-  const chromeVersion = chromeMatch ? parseInt(chromeMatch[1] ?? "", 10) : 0;
-  const isChrome = chromeVersion !== 0;
-
-  const canRender = canRenderApp();
-  const banner = (
-    <CompatibilityBanner
-      isChrome={isChrome}
-      currentVersion={chromeVersion}
-      isDismissable={canRender}
-    />
-  );
-
-  if (!canRender) {
-    // eslint-disable-next-line react/no-deprecated
-    ReactDOM.render(
-      <StrictMode>
-        <LogAfterRender>
-          <CssBaseline>{banner}</CssBaseline>
-        </LogAfterRender>
-      </StrictMode>,
-      rootEl,
-    );
-    return;
-  }
-
   // Use an async import to delay loading the majority of studio-base code until the CompatibilityBanner
   // can be displayed.
   const { installDevtoolsFormatters, overwriteFetch, waitForFonts, initI18n, StudioApp } =
@@ -89,7 +59,6 @@ export async function main(getParams: () => Promise<MainParams> = async () => ({
   ReactDOM.render(
     <StrictMode>
       <LogAfterRender>
-        {banner}
         {rootElement}
       </LogAfterRender>
     </StrictMode>,
